4f71b3f0aede9556431e715fe0c467ef1a0c2ea4
Daud Dhiya <daud.dhiya31@gmail.com>
2023-11-26 21:11:49 +0700

    refactor: prettier for views

--- application/views/form_login.php
+++ application/views/form_login.php
@@ -1,15 +1,8 @@
-
-    
-
 <body class="bg-gradient-secondary">
-
   <div class="container">
-
     <!-- Outer Row -->
     <div class="row justify-content-center">
-
       <div class="col-xl-10 col-lg-12 col-md-9">
-
         <div class="card o-hidden border-0 shadow-lg my-5">
           <div class="card-body p-0">
             <!-- Nested Row within Card Body -->
@@ -20,7 +13,9 @@
                   <div class="text-center">
                     <h1 class="h4 text-gray-900 mb-4">Login User TOKO BUKU MEDIA!</h1>
                   </div>
+
                   <?php echo $this->session->flashdata('pesan'); ?>
+
                   <form method="post" action="<?php echo base_url('auth/login');?>" class="user">
                     <div class="form-group">
                       <input type="text" name="username" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="ketik username...">
@@ -30,12 +25,11 @@
                       <input type="password" name="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="ketik password...">
                       <?php echo form_error('password','<div class="text-danger small ml-2">','</div>');?>
                     </div>
-                    
                     <button type="submit" class="btn btn-info form-control">Login</button>
-                   
                   </form>
+
                   <hr>
-                  
+
                   <div class="text-center">
                     <a class="small" href="<?php echo base_url('registrasi/index');?>">Daftar akun baru!</a>
                   </div>
@@ -44,15 +38,7 @@
             </div>
           </div>
         </div>
-
       </div>
-
     </div>
-
   </div>
-
-  
-
 </body>
-
-</html>
--- application/views/registrasi.php
+++ application/views/registrasi.php
@@ -1,14 +1,9 @@
-
-
 <body class="bg-gradient-primary">
-
   <div class="container">
-
     <div class="card o-hidden border-0 shadow-lg my-5 col-lg-6 mx-auto">
       <div class="card-body p-0">
         <!-- Nested Row within Card Body -->
         <div class="row">
-         
           <div class="col-lg">
             <div class="p-5">
               <div class="text-center">
@@ -31,14 +26,12 @@
                   </div>
                   <div class="col-sm-6">
                     <input type="password" name="password_2" class="form-control form-control-user" id="exampleRepeatPassword" placeholder="Ulangi Password">
-                     
                   </div>
                 </div>
                 <button type="submit" class="btn btn-primary btn-user btn-block">Daftar</button>
-               
               </form>
               <hr>
-              
+
               <div class="text-center">
                 <a class="small" href="<?php echo base_url('auth/login'); ?>">Sudah punya akun? Silahkan Login!</a>
               </div>
@@ -47,7 +40,5 @@
         </div>
       </div>
     </div>
-
   </div>
-
- 
+</body>
