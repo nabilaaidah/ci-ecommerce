31eab0a6183d627a19e5533be9ddac46ed7fa680
Daud Dhiya <daud.dhiya31@gmail.com>
2023-11-26 21:11:49 +0700

    refactor: prettier for views

--- application/views/proses_pesanan.php
+++ application/views/proses_pesanan.php
@@ -1,6 +1,8 @@
 <div class="container-fluid">
     <div class="alert alert-success">
-    <h3 class="text-center">Pesanan Anda Telah Berhasil Di Proses</h3>
+        <h3 class="text-center">Pesanan Anda Telah Berhasil Di Proses</h3>
     </div>
-    <a href="<?php echo base_url('dashboard/index'); ?>"><div class="btn btn-warning">Selesai</div></a>
+    <a href="<?php echo base_url('dashboard/index'); ?>">
+        <div class="btn btn-warning">Selesai</div>
+    </a>
 </div>
